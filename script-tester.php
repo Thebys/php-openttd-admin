4a1af927bc462e0744c2839b86dde3f580ddcd86
Tomáš "Thebys" Biheler <tomas.biheler@impacthub.cz>
2024-12-09 23:29:37 +0100

    Add some commands

--- script-tester.php
+++ script-tester.php
@@ -4,15 +4,31 @@ require 'src/bootstrap.php';
 $commandTemplates = [
     'get_company_name' => [
         'name' => 'Get Company Name | GSCompany.GetName | [0]',
-        'command' => '{"action": "call", "number": 123456789, "method": "GSCompany.GetName","args": [0]}'
+        'command' => '{"action": "call", "method": "GSCompany.GetName","args": [0]}'
+    ],
+    'get_company_balance' => [
+        'name' => 'Get Company Balance | GSCompany.GetBankBalance | [0]',
+        'command' => '{"action": "call", "method": "GSCompany.GetBankBalance","args": [0]}'
+    ],
+    'get_clients_list' => [
+        'name' => 'Get Clients List | GSClientList | []',
+        'command' => '{"action": "call", "method": "GSClientList","args": []}' //weird output like [1]
+    ],
+    'get_client_name' => [
+        'name' => 'Get Client Name | GSClient.GetName | [1]',
+        'command' => '{"action": "call", "method": "GSClient.GetName","args": [1]}'
+    ],
+    'get_game_climate' => [
+        'name' => 'Get Game Climate | GSGame.GetLandscape | []',
+        'command' => '{"action": "call", "method": "GSGame.GetLandscape","args": []}'
     ],
     'pause_game' => [
         'name' => 'Pause Game | GSGame.Pause | []',
-        'command' => '{"action": "call", "number": 123456789, "method": "GSGame.Pause","args": []}'
+        'command' => '{"action": "call", "method": "GSGame.Pause","args": []}'
     ],
     'unpause_game' => [
         'name' => 'Unpause Game | GSGame.Unpause | []',
-        'command' => '{"action": "call", "number": 123456789, "method": "GSGame.Unpause","args": []}'
+        'command' => '{"action": "call", "method": "GSGame.Unpause","args": []}'
     ]
 ];
 ?>
